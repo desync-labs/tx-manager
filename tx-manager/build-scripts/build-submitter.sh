c674feea49665d3eaca124d89e115606381317b6
tarunsharma <tarun.kr.sharma@hotmail.com>
2025-02-18 18:10:22 +0400

    updated submitter build script

--- tx-manager/build-scripts/build-submitter.sh
+++ tx-manager/build-scripts/build-submitter.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 # Define the version number as a constant
-VERSION="0.0.1.1-dev"
-NAME="com.fathom.tx-manager.submitter"
-ORG="intothefathom"
+VERSION="0.0.1-dev"
+APP_NAME="com.fathom.tx-manager.submitter"
+ORG_NAME="intothefathom"
 
 echo 'Changing directory to node apps'
 
-cd ../../submitter
+cd ../submitter
 
 echo `pwd`
-echo "Creating image... ${APP_NAME}:${VERSION}"
+echo "Creating image... ${ORG_NAME}/${APP_NAME}:${VERSION}"
 
 docker build -t $ORG_NAME/$APP_NAME:$VERSION .
 echo 'submitter image created.'
 
 # Output the version number to be used in the GitHub Action
 echo "SUBMITTER_VERSION=$VERSION" >> $GITHUB_ENV
-echo "SUBMITTER_NAME=$NAME" >> $GITHUB_ENV
-echo "ORG_NAME=$ORG" >> $GITHUB_ENV
+echo "SUBMITTER_NAME=$APP_NAME" >> $GITHUB_ENV
+echo "ORG_NAME=$ORG_NAME" >> $GITHUB_ENV
 
 cd ..
 
